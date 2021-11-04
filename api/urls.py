02c97dce27e931aac564b389276a0cae250e65c9
Lucas Caribé <lucasmoc47@gmail.com>
2021-11-04 12:34:41 -0300

    Adding get_answers, get_questions and get_categories routes

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,8 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-  path('welcome', views.welcome)
+  path('welcome', views.welcome),
+  path('getcategories', views.get_categories),
+  path('getquestions', views.get_questions),
+  path('getanswers/<int:question_id>', views.get_answers),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,50 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from random import choices
 
-@api_view(["GET"])
+from .serializers import AnswerSerializer, CategorySerializer, QuestionSerializer
+from .models import Answer, Category, Question
+
+@api_view(['GET'])
 @csrf_exempt
 @permission_classes([IsAuthenticated])
 def welcome(request):
   return JsonResponse({ 'message': 'Hello World!' })
+
+# get categories
+@api_view(['GET'])
+@csrf_exempt
+@permission_classes([IsAuthenticated])
+def get_categories(request):
+  categories = Category.objects.all()
+  serializer = CategorySerializer(categories, many=True)
+
+  return JsonResponse({ 'categories': serializer.data }, safe=False, status=status.HTTP_200_OK)
+
+# get questions
+@api_view(['GET'])
+@csrf_exempt
+@permission_classes([IsAuthenticated])
+def get_questions(request):
+  questions = Question.objects.all()
+  serializer = QuestionSerializer(questions, many=True)
+  
+  if len(serializer.data) > 5:
+    random_questions = choices(serializer.data, k=5)
+  else:
+    random_questions = serializer.data
+
+  return JsonResponse({ 'questions': random_questions }, safe=False, status=status.HTTP_200_OK)
+
+# get answers
+@api_view(['GET'])
+@csrf_exempt
+@permission_classes([IsAuthenticated])
+def get_answers(request, question_id):
+  answers = Answer.objects.filter(question_id=question_id)
+  serializer = AnswerSerializer(answers, many=True)
+
+  return JsonResponse({ 'answers': serializer.data }, safe=False, status=status.HTTP_200_OK)
